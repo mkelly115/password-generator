1afad6f1d197458aeb6b5c11cf198177beae8314
Michael Kelly <mikek222@gmail.com>
2023-10-20 21:13:38 -0400

    completed generator code

--- script.js
+++ script.js
@@ -6,43 +6,72 @@ const upperCase = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","
 const specialChar = ["!","@","#","$","%","^","&","(",")"]
 
 // Write password to the #password input
-function generatePassword() {
+function generatePassword(passwordLength, hasNumbers, hasLowerCase, hasUpperCase, hasSpecialChar) {
+  // console.log(passwordLength, hasNumbers, hasLowerCase, hasUpperCase, hasSpecialChar)
+let availableCharacters =[]
 
+  if (hasNumbers){
+    availableCharacters = availableCharacters.concat(numbers)
+  }
 
+  if (hasLowerCase){
+   availableCharacters = availableCharacters.concat(lowerCase)
+  }
+
+  if (hasUpperCase){
+   availableCharacters = availableCharacters.concat(upperCase)
+  }
+
+  if (hasSpecialChar){
+   availableCharacters = availableCharacters.concat(specialChar)
+  }
+let password = ""
+
+  if(availableCharacters.length === 0){
+
+  return ""
+}
+  for (let i = 0; i < passwordLength; i++){
+    const randomIndex = Math.floor(Math.random() * availableCharacters.length); 
+    password += availableCharacters[randomIndex]
+    console.log(i,randomIndex, availableCharacters[randomIndex])
+  }
+
+  return password
 
 }
 
 function validatePasswordLength(passwordLength){
-   console.log(passwordLength)
    if (!passwordLength){
-    console.log("emptypassword")
+    return false
+   } else if (passwordLength && (Number(passwordLength) <8 || Number(passwordLength) >128)){
+    window.alert("Please select a valid password length between 8 and 128 characters")
+    return false 
    }
-
-   if (passwordLength && (Number(passwordLength) <8 || Number(passwordLength) >128)){
-    console.log("Invalid Length")
+   else {
+    return true
    }
-
 }
 
 function writePassword() {
   const passwordLength = prompt("Enter a password length between 8 and 128 characters");
-  validatePasswordLength(passwordLength)
-//   let hasNumbers = confirm("Will this include numbers?");
-//   let hasLowerCase = confirm("Will this include lowercase letters?");
-//   let hasUpperCase = confirm("Will this include uppercase letters?");
-//   let hasSpecialChar = confirm("Will this include special characters?")
+  const validLength = validatePasswordLength(passwordLength)
+  if(validLength){
 
-//   console.log (passwordLength)
-//   console.log (hasNumbers)
-//   console.log (hasLowerCase)
-//   console.log (hasUpperCase)
+  
+  let hasNumbers = confirm("Select OK if you wish to include numbers");
+  let hasLowerCase = confirm("Select OK if you wish to include lower case letters");
+  let hasUpperCase = confirm("Select OK if you wish to include upper case letters");
+  let hasSpecialChar = confirm("Select OK if you wish to include special characters");
 
-  const password = generatePassword();
+  const password = generatePassword(passwordLength, hasNumbers, hasLowerCase, hasUpperCase, hasSpecialChar)
+
+  
   const passwordText = document.querySelector("#password");
 
   passwordText.value = password;
 
-
+   }
 }
 
 // Add event listener to generate button
